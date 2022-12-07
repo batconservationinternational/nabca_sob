6236cbcdad7d260f754d35097044663efd3d6ef1
ngoodby <46496548+ngoodby@users.noreply.github.com>
2022-12-07 12:44:10 -0800

    Update SoB_7_threatBuckets.R

--- RCode/SoB_7_threatBuckets.R
+++ RCode/SoB_7_threatBuckets.R
@@ -1,17 +1,23 @@
+get_impact_bins <- function(dataFolder, countrytoAnalyze, dataDate){
 
-
-
-files <- list.files(dataFolder, full.names = T)
-files <- files[grepl("scope_sev_quantiles_agg_", files)]
-f <- files[str_detect(files, countrytoAnalyze)]
-print(paste0('reading: ', f))
-threat_data <- readRDS(f)
-
-bin_threat <- function(val){
-  bin <- case_when(
-    val < 0.01 ~ "Negligible (<1%)",
-    val >= 1 & val < 10
-  )
-  return(bin)
+  # countrytoAnalyze <- "Mexico"
+  # dataFolder <- "/Users/ngoodby/Desktop/nabca_sob/Data/derived/AnalysisExport_20221116_Mexico"
+  
+  # Load in data
+  files <- list.files(dataFolder, full.names = T)
+  files <- files[grepl("scop_sev_quantiles_agg_*", files)]
+  f <- files[str_detect(files, countrytoAnalyze) & str_detect(files, )]
+  print(paste0('reading: ', f))
+  threat_data <- read_csv(f)
+  
+  # Bin median scope and severity values and pivot wider
+  binned_data <- threat_data %>% 
+    mutate(scope_sev_bin = case_when(
+      scope_sev == "Scope" ~ bin_scope(Median),
+      scope_sev == "Severity" ~ bin_sev(Median)
+    )) %>% select(species, threat, scope_sev, scope_sev_bin) %>% 
+    pivot_wider(names_from = scope_sev, values_from = scope_sev_bin) %>% 
+    mutate(impact_bin = bin_impact(Scope, Severity))
+  
+  return(binned_data)
 }
-bin_threat(val)
